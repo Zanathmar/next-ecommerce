0e662e9849b9aabe17f44501b79a21a6eef88c61
Hyunbin1234 <maayhelda@gmail.com>
2025-01-28 09:50:50 +0700

    first commit

---
+++ src/core/config.js
@@ -0,0 +1,7 @@
+class Config {
+    static appName = () => "L'MAGER - 메이거";
+    static baseApiUrl = () => "/////";
+    static baseApiUrl = () => this.baseUrl () + "api/";
+}
+
+export default Config;
